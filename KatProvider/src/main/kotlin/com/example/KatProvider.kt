f54848350c17b73f6e9f13edf3b9428a52cba681
PR4NJ41 <pranjal.rajput.che21@itbhu.ac.in>
2026-04-07 18:01:27 +0530

    ok
    Signed-off-by: PR4NJ41 <pranjal.rajput.che21@itbhu.ac.in>

---
+++ KatProvider/src/main/kotlin/com/example/KatProvider.kt
@@ -0,0 +1,21 @@
+package com.example
+
+import com.lagradost.cloudstream3.MainAPI
+import com.lagradost.cloudstream3.SearchResponse
+import com.lagradost.cloudstream3.TvType
+
+class KatProvider : MainAPI() { // All providers must be an instance of MainAPI
+    override var mainUrl = "https://example.org/"
+    override var name = "Kat Provider"
+    override val supportedTypes = setOf(TvType.Movie)
+
+    override var lang = "en"
+
+    // Enable this when your provider has a main page
+    override val hasMainPage = true
+
+    // This function gets called when you search for something
+    override suspend fun search(query: String): List<SearchResponse> {
+        return listOf()
+    }
+}
