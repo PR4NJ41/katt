489eebadf89c67ae31ba7a6cb459280ddda6c8a8
PR4NJ41 <pranjal.rajput.che21@itbhu.ac.in>
2026-04-07 18:04:28 +0530

    ik
    Signed-off-by: PR4NJ41 <pranjal.rajput.che21@itbhu.ac.in>

--- KatProvider/build.gradle.kts
+++ KatProvider/build.gradle.kts
@@ -10,7 +10,7 @@ cloudstream {
     // All of these properties are optional, you can safely remove any of them.
 
     description = "Kat sample provider"
-    authors = listOf("Cloudburst", "Luna712")
+    authors = listOf("Weekday")
 
     /**
     * Status int as one of the following:
